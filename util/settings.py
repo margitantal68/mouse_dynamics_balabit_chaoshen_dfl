86bb0532cb4b5f3f54bd37d7e22d2190b9473aa5
margitantal68 <margitantal68@gmail.com>
2019-01-20 14:43:13 +0200

    util package

---
+++ util/settings.py
@@ -0,0 +1,24 @@
+from util.const import DATASET, SPLIT_TYPE, DATASET_AMOUNT
+
+# Which dataset to use?
+CURRENT_DATASET = DATASET.DFL
+
+# How many data to use from the dataset
+DATASET_USAGE = DATASET_AMOUNT.FIRST1000
+
+# Use Chronological or Random order
+# CURRENT_SPLIT_TYPE = SPLIT_TYPE.RANDOM
+CURRENT_SPLIT_TYPE = SPLIT_TYPE.KEEP_ORDER
+
+# Number of samples used for score computations
+NUM_ACTIONS = 5
+
+# How many training samples to use - works with DATASET_USAGE = DATASET_AMOUNT.FIRST1000
+# should be less than 1000
+NUM_TRAINING_SAMPLES = 1000
+
+# Split factor
+TEST_SIZE = 0.33
+
+# Plot details
+PLOT_USER_AUC = False
